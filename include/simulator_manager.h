a435df08e9e909ea298195ea0e9ecfd35253faea
Arya Reais-Parsi <aryap@berkeley.edu>
2025-11-21 13:08:56 -0800

    notes about what's going on

--- include/simulator_manager.h
+++ include/simulator_manager.h
@@ -8,6 +8,22 @@
 
 #include "proto/spice_simulator.pb.h"
 
+// So you don't know how to do subprocess management and you just asked AI to
+// write it all for you? Crazy enough to work. Here's what's happening at the
+// system interface:
+//
+//  - we ask the kernel for two new pipes, and for each it returns a file
+//  descriptor for the read end and another for the write end
+//  - fork the process
+//    - in the child (the one that will run the simulator), tie stderr and
+//    stdout to the write (input) end of those pipes we just made
+//    - in the parent (the long-lived server), prepare to poll the read end of
+//    those same pipes
+//  - while the process runs, provide a mechanism to read the contents of each
+//  buffer and give the data to some external function (callback)
+//  - if we have to wait for completion, use another syscall to wait for the
+//  child process to complete
+
 namespace spiceserver {
 
 class SimulatorManager {
